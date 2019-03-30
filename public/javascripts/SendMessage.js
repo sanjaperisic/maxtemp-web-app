8bf72d2ea85197a5f1074d555f076f5add391923
Sanja <sanja996@live.com>
2019-03-30 21:07:04 +0100

    prvi

---
+++ public/javascripts/SendMessage.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var Client = require('azure-iothub').Client;
+var Message = require('azure-iot-common').Message;
+var connectionString = 'HostName=cake-hub-test.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=Ktwa17RjTKcMAfui8SYXkt3W4aqNmJS6pSnnLVQa6+g=';
+var targetDevice = 'myDevice';
+
+var serviceClient = Client.fromConnectionString(connectionString);
+
+function printResultFor(op) {
+    return function printResult(err, res) {
+      if (err) console.log(op + ' error: ' + err.toString());
+      if (res) console.log(op + ' status: ' + res.constructor.name);
+    };
+  }
+  function receiveFeedback(err, receiver){
+    receiver.on('message', function (msg) {
+      console.log('Feedback message:')
+      console.log(msg.getData().toString('utf-8'));
+    });
+  }
+  serviceClient.open(function (err) {
+    if (err) {
+      console.error('Could not connect: ' + err.message);
+    } else {
+      console.log('Service client connected');
+      serviceClient.getFeedbackReceiver(receiveFeedback);
+      var message = new Message('Cloud to device message.');
+      message.ack = 'full';
+      message.messageId = "My Message ID";
+      console.log('Sending message: ' + message.getData());
+      serviceClient.send(targetDevice, message, printResultFor('send'));
+    }
+  });
---
+++ public/javascripts/index.js
@@ -0,0 +1,139 @@
+$(document).ready(function () {
+
+'use strict'
+
+var Client = require('azure-iothub').Client;
+var Message = require('azure-iot-common').Message;
+var connectionString = 'HostName=cake-hub-test.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=Ktwa17RjTKcMAfui8SYXkt3W4aqNmJS6pSnnLVQa6+g=';
+var targetDevice = 'myDevice';
+
+var serviceClient = Client.fromConnectionString(connectionString);
+
+function printResultFor(op) {
+    return function printResult(err, res) {
+      if (err) console.log(op + ' error: ' + err.toString());
+      if (res) console.log(op + ' status: ' + res.constructor.name);
+    };
+  }
+  function receiveFeedback(err, receiver){
+    receiver.on('message', function (msg) {
+      console.log('Feedback message:')
+      console.log(msg.getData().toString('utf-8'));
+    });
+  }
+ 
+  var timeData = [],
+    temperatureData = [],
+    humidityData = [];
+  var data = {
+    labels: timeData,
+    datasets: [
+      {
+        fill: false,
+        label: 'Temperature',
+        yAxisID: 'Temperature',
+        borderColor: "rgba(255, 204, 0, 1)",
+        pointBoarderColor: "rgba(255, 204, 0, 1)",
+        backgroundColor: "rgba(255, 204, 0, 0.4)",
+        pointHoverBackgroundColor: "rgba(255, 204, 0, 1)",
+        pointHoverBorderColor: "rgba(255, 204, 0, 1)",
+        data: temperatureData
+      },
+      {
+        fill: false,
+        label: 'Humidity',
+        yAxisID: 'Humidity',
+        borderColor: "rgba(24, 120, 240, 1)",
+        pointBoarderColor: "rgba(24, 120, 240, 1)",
+        backgroundColor: "rgba(24, 120, 240, 0.4)",
+        pointHoverBackgroundColor: "rgba(24, 120, 240, 1)",
+        pointHoverBorderColor: "rgba(24, 120, 240, 1)",
+        data: humidityData
+      }
+    ]
+  }
+
+  var basicOption = {
+    title: {
+      display: true,
+      text: 'Temperature & Humidity Real-time Data',
+      fontSize: 36
+    },
+    scales: {
+      yAxes: [{
+        id: 'Temperature',
+        type: 'linear',
+        scaleLabel: {
+          labelString: 'Temperature(C)',
+          display: true
+        },
+        position: 'left',
+      }, {
+          id: 'Humidity',
+          type: 'linear',
+          scaleLabel: {
+            labelString: 'Humidity(%)',
+            display: true
+          },
+          position: 'right'
+        }]
+    }
+  }
+
+  //Get the context of the canvas element we want to select
+  var ctx = document.getElementById("myChart").getContext("2d");
+  var optionsNoAnimation = { animation: false }
+  var myLineChart = new Chart(ctx, {
+    type: 'line',
+    data: data,
+    options: basicOption
+  });
+
+  var ws = new WebSocket('wss://' + location.host);
+  ws.onopen = function () {
+    console.log('Successfully connect WebSocket');
+  }
+  ws.onmessage = function (message) {
+    console.log('receive message' + message.data);
+    try {
+      var obj = JSON.parse(message.data);
+      if(!obj.time || !obj.temperature) {
+        return;
+      }
+	  
+      timeData.push(obj.time);
+      temperatureData.push(obj.temperature);
+      // only keep no more than 50 points in the line chart
+      const maxLen = 50;
+      var len = timeData.length;
+      if (len > maxLen) {
+        timeData.shift();
+        temperatureData.shift();
+      }
+
+      if (obj.humidity) {
+        humidityData.push(obj.humidity);
+      }
+      if (humidityData.length > maxLen) {
+        humidityData.shift();
+      }
+
+      myLineChart.update();
+	   serviceClient.open(function (err) {
+    if (err) {
+      console.error('Could not connect: ' + err.message);
+    } else {
+      console.log('Service client connected');
+      serviceClient.getFeedbackReceiver(receiveFeedback);
+      var message = new Message('Cloud to device message.');
+      message.ack = 'full';
+      message.messageId = "My Message ID";
+      console.log('Sending message: ' + message.getData());
+      serviceClient.send(targetDevice, message, printResultFor('send'));
+    }
+  });
+    } catch (err) {
+      console.error(err);
+    }
+  }
+});
